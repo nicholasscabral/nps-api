99030736e4ad0131d48621e0927383dedd4b3a52
nicholasscabral <nicholasscabral@gmail.com>
2021-02-22 11:17:03 -0300

    testing first route

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({message: 'Hello world'})
+})
+
+app.post('/', (req, res) => {
+  return res.json({message: 'rota de teste'})
+})
+
 app.listen(3333, () => console.log('API running...'))
