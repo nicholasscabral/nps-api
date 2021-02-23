7d155d50e9277b2735355585e9cfb965a68c898c
nicholasscabral <nicholasscabral@gmail.com>
2021-02-23 15:59:23 -0300

    registering user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
   async create(req: Request, res: Response) {
-    const body = req.body;
-    console.log(body);
-    res.send("deu bom")
+    const { name, email } = req.body;
+
+    const usersRepository = getRepository(User)
+
+    const usersAlreadyExist = await usersRepository.findOne({ email })
+
+    if(usersAlreadyExist) {
+      return res.status(400).json({message: 'user already exists'});
+    }
+
+    const user = usersRepository.create({
+      name: name,
+      email: email
+    })
+
+    await usersRepository.save(user);
+
+    return res.json(user);
   }
 }
 
