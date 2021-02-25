7861c2c790c47196ce7c1738e2128e0a04f115de
nicholasscabral <nicholasscabral@gmail.com>
2021-02-25 19:25:25 -0300

    config email template

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,4 +1,7 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { resolve } from 'path';
+import handlebars from 'handlebars';
+import fs from 'fs';
 
 class SendMailService {
   private client: Transporter
@@ -20,12 +23,22 @@ class SendMailService {
   }
 
   async execute(to: string, subject: string, body: string) {
+    const npsPath = resolve(__dirname, "../views/emails/npsMail.hbs")
+    const templateFileContent = fs.readFileSync(npsPath).toString("utf8")
+
+    const mailTemplateParse = handlebars.compile(templateFileContent)
+
+    const html = mailTemplateParse({
+      name: to,
+      title: subject,
+      description: body
+    })
 
     const message = await this.client.sendMail({
       to,
       from: "NPS <noreply@nps.com>",
       subject,
-      html: body,
+      html: html,
     })
 
     console.log('Message sent: %s', message.messageId);
