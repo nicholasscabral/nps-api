b29033c2ce4055ad413accc5d93d9ffe9a133e45
nicholasscabral <nicholasscabral@gmail.com>
2021-02-27 11:27:59 -0300

    create NpsController

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { UserController } from './controllers/UserController'
 import { SurveysController } from './controllers/SurveysController'
 import { SendMailController } from './controllers/SendMailController'
 import { AnswerController } from './controllers/AnswerController'
+import { NpsController } from './controllers/NpsController'
 
 const router = Router()
 
@@ -10,11 +11,13 @@ const userController = new UserController()
 const surveyController = new SurveysController()
 const sendMailController = new SendMailController()
 const answerController = new AnswerController()
+const npsController = new NpsController()
 
 router.post('/users', userController.create)
 router.post('/surveys', surveyController.create)
 router.get('/surveys', surveyController.show)
 router.post('/sendMail', sendMailController.execute)
 router.get('/answers/:value', answerController.execute)
+router.get('/nps/:survey_id', npsController.execute)
 
 export { router }
